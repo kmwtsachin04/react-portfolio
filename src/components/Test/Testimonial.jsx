f588caa0b2a4379a97d097d8ee9c99cbb5fd3723
Sachin Kumawat <sachin09688@gmail.com>
2023-09-19 22:43:44 +0530

    second commit

--- src/components/Test/Testimonial.jsx
+++ src/components/Test/Testimonial.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./tesimonial.css";
 import AVTR1 from "../../assets/avatar1.png";
 import AVTR2 from "../../assets/avatar2.jpg";
-import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR3 from "../../assets/avatar3.png";
 import AVTR4 from "../../assets/avatar4.jpg";
+import AVTR5 from "../../assets/avatar5.jpg";
+import AVTR6 from "../../assets/avatar6.jpg";
+import AVTR7 from "../../assets/avatar7.jpg";
 
 
 // import Swiper core and required modules
@@ -17,26 +20,41 @@ import 'swiper/swiper-bundle.css'; // Use 'swiper/swiper-bundle.css' for the bun
 const data = [
   {
       avatar: AVTR1,
-      name: 'Sandeep Mahawar',
+      name: 'Vijay Choudhary',
       review: 'The web development service by Sachin Kumawat is top-notch! He is highly skilled, professional, and efficient. He was able to deliver the project on time and exceeded our expectations. I highly recommend him for any web development needs.'
   },
    {
       avatar: AVTR2,
-      name: "Rajveer Kumawat",
-      review: 'I highly recommend Sachin Kumawat for all your UI design and wireframing needs. He is incredibly talented and has a keen eye for detail. He has been able to bring our vision to life with ease and has exceeded our expectations. He is a true professional and we look forward to working with him again in the future.'
+      name: "Dharmveer Choudhay",
+      review: 'The web development service provided by Sachin Kumawat is of exceptional quality. He possesses a high level of skill, professionalism, and efficiency. Furthermore, he was able to deliver the project within the designated timeframe and surpassed our expectations. I strongly endorse him for any web development requirements.'
    },
    {
       avatar: AVTR3,
-      name: "Tanishq Sharma",
+      name: "Sandeep Mahawar",
       review: 'The web development service by Sachin Kumawat is top-notch! He is highly skilled, professional, and efficient. He was able to deliver the project on time and exceeded our expectations. I highly recommend him for any web development needs.'
 
 
   },
    {
       avatar: AVTR4,
-      name: "Rahul Kumawat",
-      review: 'I highly recommend Sachin Kumawat for all your UI design and wireframing needs. He is incredibly talented and has a keen eye for detail. He has been able to bring our vision to life with ease and has exceeded our expectations. He is a true professional and we look forward to working with him again in the future.'
+      name: "Anandpal Singh Rathore",
+      review: 'The web development service provided by Sachin Kumawat is of exceptional quality. He possesses a high level of skill, professionalism, and efficiency. Furthermore, he was able to deliver the project within the designated timeframe and surpassed our expectations. I strongly endorse him for any web development requirements.'
   },
+  {
+    avatar: AVTR5,
+    name: "Rajveer Kumawat",
+    review: 'I highly recommend Sachin Kumawat for all your UI design and wireframing needs. He is incredibly talented and has a keen eye for detail. He has been able to bring our vision to life with ease and has exceeded our expectations. He is a true professional and we look forward to working with him again in the future.'
+},
+{
+  avatar: AVTR6,
+  name: "Sunil Kumar",
+  review: 'I strongly endorse Sachin Kumawat for any requirements related to UI design and wireframing. His exceptional talent and meticulous attention to detail are truly remarkable. He has effortlessly transformed our vision into reality and surpassed our expectations. He is an epitome of professionalism and we eagerly anticipate collaborating with him in the future.'
+},
+{
+  avatar: AVTR7,
+  name: "Tanishq Sharma",
+  review: 'For all issues related to UI design and wireframing, contact Sachin Kumawat. His extraordinary talent and meticulous attention to detail are truly remarkable. He effortlessly turned our vision into reality and exceeded our expectations. He is the epitome of professionalism and we look forward to working with him in the future.'
+},
 ]
 
 const Testimonial = () => {
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import HeaderSocial from './HeaderSocial'
 import {useTypewriter, Cursor} from 'react-simple-typewriter'
 
 const Header = () => {
-  const [text] = useTypewriter ({
+  const [text] = useTypewriter ({ 
     words:['Fullstack Developer',
             "Web Developer",
             "Backend Developer",
@@ -22,8 +22,8 @@ const Header = () => {
         <div className="container header_container">
             <h5>Hello I'm</h5>
             <h1>Sachin Kumawat</h1>
-            <h5 className='text-light'>I am a {text}
-            <span><Cursor cursorStyle="|"/></span>
+            <h5 className='text-lightt'> <span className='role'>I'm a </span>{text}
+            <span className='role'>< Cursor cursorStyle="|"/></span>
             </h5>
             <CTA/>
             <HeaderSocial/>
